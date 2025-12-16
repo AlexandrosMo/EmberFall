8115b09bb5076632e489f18b3339630a52a0121e
AlexandrosMo <alexandros.moraitis99@gmail.com>
2025-12-16 15:51:23 +0200

    main.py changes

--- EmberFall/main.py
+++ EmberFall/main.py
@@ -1,8 +1,7 @@
 import sys
 import os
 import yaml
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import time
 
 from Player.player import create_player_interactive
 
@@ -16,29 +15,44 @@ class EmberFallGame():
     def __init__(self):
         options = input("WELCOME TO EMBERFALL \n 1. Start New Game \n 2. Load Game \n Choose an option: ")
         if options == "1":
-            
         # create player interactively (prompts for name/class)
             self.player = create_player_interactive()
             print(f"Welcome, {self.player.name}, to the world of EmberFall!")
-            start= input("Press any key to begin your adventure...")
+            start = input("Press Enter to begin your adventure...")
             if start == "":
-                print(intro_data)
-            elif start != "":
-                print(input("press Enter Bro.."))
-                if start != "":
-                    print(input("if you dont press enter you cant play.."))
-                    if start != "":
-                        print(input("if you press any other key you are gay.."))
-                        if start != "":
-                            print(input("just press enter smh.."))
-                            if start != "":
-                                print("Nevermind, Good Luck Stupid!..")
+                print(f"Welcome, {self.player.name}, to the world of EmberFall!")
+                for c in intro_data:
+                        sys.stdout.write(c)
+                        sys.stdout.flush()
+                        time.sleep(0.05)
+                        if c == ".":
+                            sys.stdout.write(c)
+                            sys.stdout.flush()
+                            time.sleep(0.05)
+                            enter = input("\npress enter to continue or s to skip...")
+                            print (enter)
+                            if enter == press("s"):
+                                break
+                print(c)
 
-            self.start_new_game()
-           
-        elif options == "2":
+            else:
+                for start in start:
+                    if start != "enter":
+                        print(input("press Enter Bro.."))
+                        if start != "enter":
+                            print(input("if you dont press enter you cant play.."))
+                            if start != "enter":
+                                print(input("if you press any other key you are gay.."))
+                                if start != "enter":
+                                    print(input("just press enter smh.."))
+                                    if start != "enter":
+                                        print("Nevermind, Good Luck Stupid!..")
+
+                    print(f"Welcome, {self.player.name}, to the world of EmberFall!")
+
+        else:
+            options == "2"
             self.load_game()
-         
 
     def load_game(self):
         self.player = save_system.load_game()
